b3514e8ae5357bf6ee69a3f5feb0be8a377c7441
joaomarri <joaomarri@gmail.com>
2015-07-26 17:26:07 -0300

    versionando inicial

---
+++ README.md
@@ -0,0 +1,8 @@
+# PopularShots
+
+Client-side application, Angularjs, in order to test the Dribbble API and show the most popular shots:
+
+Example popular shots: http://api.dribbble.com/shots/popular?page=1
+
+Example of the shot's detail: http://api.dribbble.com/shots/1757954
+
---
+++ js/main.js
@@ -0,0 +1,117 @@
+
+/**
+ * AngularJS Web Application
+ */
+var app = angular.module('WebApp', ['ngRoute', 'ngResource', 'ui.bootstrap', 'ngAnimate', 'ngSanitize', 'mgcrea.ngStrap']);
+
+/**
+ * Configure the Routes
+ */
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    // Home
+    .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+    // Pages
+    .when("/about", {templateUrl: "partials/about.html"})
+    .when("/detail/:id", {templateUrl: "partials/detail.html", controller: "DetailCtrl"})
+	.when("/contact", {templateUrl: "partials/contact.html"})
+    .otherwise({
+       redirectTo: "/"
+     });
+}]);
+
+
+app.config(function($asideProvider) {
+  angular.extend($asideProvider.defaults, {
+    container: 'body',
+    html: true
+  });
+});
+
+app.factory("ShotsPage", function($resource) {
+	var api = $resource("http://api.dribbble.com/shots/popular?page=:page",
+    { callback: "JSON_CALLBACK" },
+    { get: { method: "JSONP" }});
+	
+	return api;
+});
+
+app.factory("ShotDetail", function($resource) {
+	var api = $resource("http://api.dribbble.com/shots/:id",
+    { callback: "JSON_CALLBACK" },
+    { get: { method: "JSONP" }});
+	
+	return api;
+});
+
+
+ app.directive('loading',   ['$http' ,function ($http) {
+        return {
+            restrict: 'A',
+            link: function (scope, elm, attrs)
+            {
+                scope.isLoading = function () {
+                    return $http.pendingRequests.length > 0;
+                };
+
+                scope.$watch(scope.isLoading, function (v)
+                {
+                    if(v){
+                        elm.show();
+                    }else{
+                        elm.hide();
+                    }
+                });
+            }
+        };
+
+ }]);
+
+/**
+ * Controls all other Pages
+ */
+app.controller('PageCtrl', function ($scope, $location, $http, ShotsPage) {
+  console.log("Page Controller reporting.");
+    
+    $scope.currentPage = 1;
+    ShotsPage.get({ page: $scope.currentPage }, function (data) {
+      $scope.shots = data.shots;
+    });
+    
+  $scope.showDetail = function(idShot) {
+		console.log(idShot);
+		$location.path("/detail/"+idShot);
+	}
+  
+  $scope.totalItems = 500;
+  $scope.currentPage = 1;
+   $scope.paginaService = ShotsPage;
+
+  $scope.setPage = function (pageNo) {
+    $scope.currentPage = pageNo;
+  };
+
+  $scope.pageChanged = function() {
+    console.log('Page changed to: ' + $scope.currentPage);
+      $scope.paginaService.get({page: $scope.currentPage}, function (data) {
+          $scope.shots = data.shots;
+      });
+  };
+
+  $scope.maxSize = 5;
+ 
+  
+  $scope.aside = {title: 'Title', content: 'Hello Aside<br />This is a multiline message to Joao!'};
+
+  $scope.asideContato = {title: 'Contato'};
+    
+});
+
+app.controller('DetailCtrl', function ($scope, $location, $http, $route, ShotDetail) {
+  console.log("Detail Controller reporting.");
+
+  ShotDetail.get({ id: $route.current.params.id }, function (data) {
+      $scope.shot = data;
+    });
+});
+
